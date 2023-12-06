f0f8c65f98fd22df4f6b1f90da1c45c70eae1466
David <davcg5@hotmail.com>
2023-12-05 21:11:23 -0500

    auth setup

---
+++ dev-flow/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <main
+    className='flex min-h-screen w-full
+    items-center justify-center'
+    >
+        SignIN
+    </main>
+  )
+}
+
+export default SignIn
